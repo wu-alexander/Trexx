ec7b5bdb481f027443230e6f0503c1ec343e4802
Alex Wu <alexander.wu14@gmail.com>
2022-05-24 18:03:44 -0500

    generated bookings controller and added new and create routes

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -1,3 +1,4 @@
 class Trek < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :treks
+  resources :treks do
+    resources :bookings, only: [:new, :create]
+  end
 end
