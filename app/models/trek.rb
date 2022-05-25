954d6db31ae77c28c1858958e3eedee1092195d6
Alex Wu <alexander.wu14@gmail.com>
2022-05-25 17:15:17 -0500

    added validation

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -1,4 +1,6 @@
 class Trek < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, :description, :start_location, :difficulty, :duration_days, :price, presence: true
 end
