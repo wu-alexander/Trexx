cd23ea5fb97c801be20f5047aab99dc2e8534404
Alex Wu <alexander.wu14@gmail.com>
2022-05-27 00:51:30 -0500

    able to see index and show even when not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
   include Pundit
 
   # Pundit: white-list approach.
